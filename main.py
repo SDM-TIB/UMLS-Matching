f2b8c318d72ed38a68802b0db7cd1cde3ba9f158
Ahmad Sakor <sakerobs@gmail.com>
2021-02-04 19:51:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import csv
+import editdistance
 
 f = open("data/CUIs_oneLabel.csv", 'r',encoding="utf-8" )
 reader = csv.reader(f,delimiter=',')
 cuis=list(reader)
+f.close()
+
+
+f = open("data/CUIs.csv", 'r',encoding="utf-8" )
+reader = csv.reader(f,delimiter=',')
+cuis_lang=list(reader)
 f.close()
 
     
@@ -17,6 +24,10 @@ def get_cui_id(label):
     label=laberl.lower().strip().replace('_',' ')
     if label in CUIsLabel
         return CUIsLabel[label]
+    else:
+        for row in cuis_lang:
+            if editdistance.eval(label, row[1])<=1
+                return row[0]
     
 def get_cui_label(id):
   if label in CUIs
