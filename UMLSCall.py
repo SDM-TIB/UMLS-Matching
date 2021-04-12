82b75c5e8e38054db56a3e53dfcb6d92b8b4ee17
Ahmad Sakor <sakerobs@gmail.com>
2021-04-12 16:49:36 +0200

    Update UMLSCall.py

--- UMLSCall.py
+++ UMLSCall.py
@@ -11,17 +11,27 @@ def getLabel(text):
         response=r.json()
         return response['label']
     else:
-        return -1
+        return ""
 
 
-def getCui(text):
-    url = 'https://labs.tib.eu/sdm/umlsmatching/umlsmatching?type=cui'
-    payload = '{"data":"'+text+'"}'
-    r = requests.post(url, data=payload.encode('utf-8'), headers=headers)
-    if r.status_code == 200:
-        response=r.json()
-        return response['cui']
-    else:
-        return -1
 
+def getCui(text, mode='short'):
+    if mode=='short':
+        url = 'https://labs.tib.eu/sdm/umlsmatching/umlsmatching?type=cui'
+        payload = '{"data":"'+text+'"}'
+        r = requests.post(url, data=payload.encode('utf-8'), headers=headers)
+        if r.status_code == 200:
+            response=r.json()
+            return response['cui']
+        else:
+            return ""
+    elif mode=='long':
+        url = 'http://node3.research.tib.eu:5005/api_umls?mode=long&type=cui'
+        payload = '{"data":"'+text+'"}'
+        r = requests.post(url, data=payload.encode('utf-8'), headers=headers)
+        if r.status_code == 200:
+            response=r.json()
+            return response['entities']
+        else:
+            return ""
 
